cbfc147f7737d7aa783a02c276048c637db72c52
Andriel Marafigo <165610954+Andrielmarafigo@users.noreply.github.com>
2025-05-22 19:53:00 -0300

    Update formulario.js

--- forms/formulario.js
+++ forms/formulario.js
@@ -1,77 +1,111 @@
 class Contato {
-    //Atributos
-    constructor() {
-      this.nome = null;
-      this.email = null;
-      this.fone = null;
-      this.mensagem = null;
-      this.data_cadastro = null;
-      this.tipo_contato = null;
-    }
-  
-    // Métodos
-    Enviar(event) {
-        event.defaultPrevented;
-      let alerta = '';
 
-        console.log = 'Cheguei aqui'
-
-      //if (nome esta vazio) mandar uma mensagem de erro:
-      this.nome = document.getElementById('nome').value;
-      if (this.nome === ''){
-        alerta = 'nome';
-      }
-  
-      this.email = document.getElementById('email').value;
-      if (this.email === ''){
-        alerta += ', email';
-      }
-  
-      this.fone = document.getElementById('fone').value;
-      if (this.fone === ''){
-        alerta += ', fone';
-      } 
-  
-      this.mensagem = document.getElementById('mensagem').value;
-      if (this.mensagem === ''){
-        alerta += ', mensagem';
-      }
-  
-      this.data_cadastro = new Date();
-      if (this.data_cadastro === ''){
-        alerta += ', data_cadastro';
-      }
-  
-      this.tipo_contato = document.getElementById('tipo_contato').value;
-      if (this.tipo_contato === ''){
-        alerta += ', tipo_contato';
-      }
-  
-      if (alerta != '') {
-        alert('Os seguintes campos estão com problema: ' +alerta);
-      }
-  
-      let tabela = document.getElementById('dados');
-      let linha = tabela.insertRow();
-      linha.insertCell();
-      linha.innerHTML = this.nome;
-      linha.insertCell();
-      linha.innerHTML = this.email;
-      linha.insertCell();
-      linha.innerHTML = this.fone;
-      linha.insertCell();
-      linha.innerHTML = this.mensagem;
-      linha.insertCell();
-      linha.innerHTML = this.tipo_contato;    
-  
-    }
+  // Atributos
+  constructor() {
+    this.nome = null;
+    this.email = null;
+    this.fone = null;
+    this.mensagem = null;
+    this.data_cadastro = null;
+    this.tipo_contato = null;
   }
-  // Instanciar a classe
-  contato = new Contato();
   
-  //function submeter(){
-  //    var alerta = document.getElementById('nome').value;
-  //    alert('Aviso: '+ alerta);
-  //}
-  
-  
+  // Métodos
+  Enviar(event) {
+    event.preventDefault();
+
+    let alerta = '';
+
+    console.log('Enviar: Preenchendo atributos');
+
+    this.nome = document.getElementById('nome').value;
+    if (this.nome === '') {
+      alerta = 'nome';
+    }
+
+    this.email = document.getElementById('email').value;
+    if (this.email === '') {
+      alerta += ', email';
+    }
+
+    this.fone = document.getElementById('fone').value;
+    if (this.fone === '') {
+      alerta += ', fone';
+    }
+
+    this.mensagem = document.getElementById('mensagem').value;
+    if (this.mensagem === '') {
+      alerta += ', mensagem';
+    }
+
+    this.data_cadastro = new Date();
+    if (this.data_cadastro === '') {
+      alerta += ', data de cadastro';
+    }
+
+    // Captura o tipo de contato selecionado
+    let radios = document.getElementsByName('tipo_contato');
+    for (let i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            this.tipo_contato = radios[i].value;
+            break;
+        }
+    }
+    if (this.tipo_contato === '')
+      this.tipo_contato = 'Indefinido';
+
+    if (alerta != '') {
+     alert('Os seguintes campos estão com problema: '+alerta);
+    } 
+
+    console.log('Enviar: Preenchendo o grid');
+
+    let tabela = document.getElementById('dados');
+    let linha = tabela.insertRow();
+
+    let col1 = linha.insertCell(0);
+    col1.textContent = this.nome;
+
+    let col2 = linha.insertCell(1);
+    col2.textContent = this.email;
+
+    let col3 = linha.insertCell(2);
+    col3.textContent = this.fone;
+
+    let col4 = linha.insertCell(3);
+    col4.textContent = this.mensagem;
+
+    let col5 = linha.insertCell(4);
+    col5.textContent = this.data_cadastro;
+
+    let col6 = linha.insertCell(5);
+    col6.textContent = this.tipo_contato;
+
+    let col7 = linha.insertCell(6);
+    col7.innerHTML = `
+            <button onClick="contato.Editar()">Editar</button>
+            <button onClick="contato.Deletar()">Deletar</button>
+        `;
+  }
+
+  // Editar
+  Editar(){
+    console.log('Editando vamos construir');
+    alert('Editando ainda sera criado');
+  }
+
+  // Deletar
+  Deletar(){
+    alert('Deletando ainda sera criado');
+  }
+}
+
+// Instanciar a classe
+let contato = new Contato(); 
+
+/*
+function submeter(){
+  var alerta = document.getElementById('nome').value;
+  alert('Aviso: '+ alerta);
+}
+*/
